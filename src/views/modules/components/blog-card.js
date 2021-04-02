84b4932c20ca3dd24d0fc1965518d02385f1207b
ShawnHosea <shawnhosea@shawnhosea.com>
2021-04-02 12:28:34 -0400

    style card

--- src/views/modules/components/blog-card.js
+++ src/views/modules/components/blog-card.js
@@ -1,144 +1,17 @@
-import listFromObject from '../helpers/list.js'
-import slugify from '../helpers/slugify.js'
 
-const map = {
-  list: function List (state = {}) {
-    let { children } = state
-    return `
-<ul
-  class="
-    mb1
-    list-none
-  "
->
-  ${children}
-</ul>
-    `
-  },
-  item: function Item (state = {}) {
-    let { child = '', children = [], depth, path, active } = state
-    let isHeading = children.length
-    let ml = depth > 1
-      ? 'ml-5'
-      : ''
-    return `
-<li
-  class="
-    mb-1
-    ${ml}
-  "
->
-  ${
-  isHeading
-    ? Heading({ children: child, depth, path, active })
-    : Anchor({ children: child, depth, path, active })
-}
-  ${children}
-</li>
-    `
-  }
-}
-
-function Anchor (state = {}) {
-  let { children, path, active } = state
-  let uri = path
-    .concat([ children ])
-    .map(part => slugify(part))
-    .join('/')
-  let href = `/${uri}`
-  let isActive = active === href
-  let activeClass = isActive
-    ? 'active'
-    : ''
-  let pointer = isActive
-    ? '→'
-    : ''
-
-  return `
-<a href="${href}" class="w-full inline-block text-p1 text-h1 text-a2 no-underline font-medium ${activeClass}" >${pointer} ${children}</a>
-  `
-}
-
-function Heading3 (state = {}) {
-  let { children } = state
-  return `
-<h3
-  class="
-   mb-5
-   font-semibold
-   text1
-  "
->
-  ${children}
-</h3>
-<hr class="border-solid border1 border-g10 mb1">
-  `
-}
-
-function Heading4 (state = {}) {
-  let { children } = state
-  return `
-<h4
-  class="
-   mb0
-   text0
-   font-semibold
-  "
->
-  ${children}
-</h4>
-  `
-}
-
-function Heading5 (state = {}) {
-  let { children } = state
-  return `
-<h5
-  class="
-   mb-1
-   text0
-   font-medium
-  "
->
-  ${children}
-</h5>
-  `
-}
-
-function Heading (state = {}) {
-  let { depth } = state
-  const headings = [
-    Heading3,
-    Heading4,
-    Heading5
-  ]
-  return headings[depth - 1](state)
-}
 
 export default function BlogCard (props = {}) {
-  let { active } = props
+  console.log(props.posts)
+  
   return `
-<aside
-  id="sidebar"
-  class="
-    h-full
-    fixed
-    left-sidebar
-    static-lg
-    pt3
-    pr2
-    pb3
-    pl2
-    overflow-auto
-    sidebar-w
-    transition-x
-    col-start-1
-    col-end-2
-    row-start-2
-    bg-g0
-  "
->
-  <ul>${ props.posts.map(p=> `<li><a href=/posts/${p}>${p}</a>`).join('') }</ul>
-</aside>
+<div>
+  <div class="postsGrid">${ props.posts.map(post => `
+    <div class="postCard">
+      <div class="postIMG"><img src=''/></div>
+      <p><a href=/posts/${post}>${post}</a></p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>`).join('') }
+  </div>
+</div>
   `
 }
