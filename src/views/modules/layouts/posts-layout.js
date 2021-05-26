29ed46fbf7e3727ffb6c80197de4c8927fdf425a
ShawnHosea <shawnhosea@shawnhosea.com>
2021-05-26 11:10:57 -0400

    fixed blog post img for sure

--- src/views/modules/layouts/posts-layout.js
+++ src/views/modules/layouts/posts-layout.js
@@ -91,7 +91,7 @@ export default function postsLayout (props = {}) {
           "
         >
         </h1>
-        <img src=${image} alt='blogimg' height="500"/>
+        <img src=${image} alt='blogimg' style='width:100%;'/>
         ${children}
         
       </div>
